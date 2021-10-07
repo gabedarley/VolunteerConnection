90407397cbc73f99752a29bb508d0591bd6a7ad0
gabedarley <gdarley@tulane.edu>
2021-10-07 14:34:52 +0000

    message

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,32 +24,48 @@ organizations = [
     :contact_phone => '504-300-1132', 
     :contact_email => 'info@growdatyouthfarm.org',
     :category => 'garden'},
-  {:name => 'Take Paws Rescue'
-    :address => '2730 Banks St, New Orleans, LA 70119'
-    :web_address => 'https://www.takepawsrescue.org/'
-    :contact_phone => 'n/a'
-    :contact_email => 'Adopt@takepawsrescue.org'
-    :category => 'rescue'
-  },
+  {:name => 'Take Paws Rescue',
+    :address => '2730 Banks St, New Orleans, LA 70119',
+    :web_address => 'https://www.takepawsrescue.org/',
+    :contact_phone => 'n/a',
+    :contact_email => 'Adopt@takepawsrescue.org',
+    :category => 'rescue'},
   {:name => 'Habitat for Humanity',
     :address => '2900 Elysian Fields Ave, New Orleans, LA 70122',
     :web_address => 'https://habitat-nola.org/',
     :contact_phone => '504-861-2077',
     :contact_email => 'info@habitat-nola.org',
     :category => 'housing'},
-  {:name => 'Audobon Nature Institute'
-   :address => '6500 Magazine St, New Orleans, LA 70118'
-   :web_address => 'https://audubonnatureinstitute.org/'
-   :contact_phone => '504-212-5377'
-   :contact_email => 'volunteer@auduboninstitute.org'
-   :category => 'nature'
-  }
-  {:name =>
-   :address =>
-   :web_address =>
-   :cont
-    
-  }
+  {:name => 'Audobon Nature Institute',
+   :address => '6500 Magazine St, New Orleans, LA 70118',
+   :web_address => 'https://audubonnatureinstitute.org/',
+   :contact_phone => '504-212-5377',
+   :contact_email => 'volunteer@auduboninstitute.org',
+   :category => 'nature'},
+  {:name => 'Cajun Navy Relief',
+   :address => 'Cajun Navy Relief, Inc. PO Box 791632 New Orleans, LA 70179',
+   :web_address => 'https://www.cajunnavyrelief.com/',
+   :contact_phone => 'info@cajunnavyrelief.com',
+   :contact_email => '504-517-NAVY',
+   :category => 'relief' },
+  {:name => 'Heart N Hands',
+   :address => '4460 Saint Bernard Ave, New Orleans, LA 70122',
+   :web_address => 'https://www.heartnhands.org/',
+   :contact_phone => 'info@heartnhands.org',
+   :contact_email => '800-276-8082',
+   :category => 'health'},
+  {:name => 'Edible Schoolyard',
+   :address => '300 N Broad St, New Orleans, LA 70119',
+   :web_address => 'https://esynola.org/',
+   :contact_phone => 'amelia@esynola.org',
+   :contact_email => '504-267-9038',
+   :category => 'garden' },
+  {:name => 'Nola 4 Women',
+   :address => '5208 Magazine St #164, New Orleans, LA 70115',
+   :web_address => 'https://www.nola4women.org/',
+   :contact_phone => 'n/a',
+   :contact_email => 'kathy@nola4women.org',
+   :category => 'gender equity'}
 ]
 
 organizations.each do |org|
