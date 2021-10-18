49092958984f9d1bb5262ccc8e5ac652d87b7683
gabedarley <gdarley@tulane.edu>
2021-10-18 06:34:34 +0000

    Bootstrap table improvements

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :organizations, :events
-  
+  get 'home/index', to: 'static_pages#home', as: 'home'
 #   get "home/index"
    
 end
