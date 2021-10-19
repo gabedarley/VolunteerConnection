d624be283d7ce0fa5ac2b89106470cb5574e9644
gabedarley <gdarley@tulane.edu>
2021-10-19 18:38:50 +0000

    Updated validation and Bootstrap for Organizations

--- app/models/Organization.rb
+++ app/models/Organization.rb
@@ -1,2 +1,7 @@
 class Organization < ActiveRecord::Base
-end
+  validates :name, :presence => true
+  validates :web_address, :presence => true
+  validates :category, :presence => true
+  validates :contact_phone, :format => { :with => /\A[2-9]\d{2}-\d{3}-\d{4}\z/ }
+  validates :contact_email, :format => { :with => /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\z/i }
+  end
